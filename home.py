47c674a4316c6a025b9df0b4060a359aa32c9c50
sunday <shivamghodke10000@gmail.com>
2022-05-29 14:58:43 +0530

    initial commit

--- home.py
+++ home.py
@@ -24,6 +24,7 @@ with st.expander("Hand gesture recogniton (05/2021 - 07/2021)"):
 with st.expander("Covid Data Analysis (02/2021 - 05/2021)"):
     st.write("Description: Used matplotlib and seaborn to display beautiful visualisation of covid19 data, and machine learning to predict the number of cases over a certain time period")
     st.write("Technologies Used: pandas, numpy, matplotlib, seaborn , machine learning, sklearn (linear regression, polynomial regression")
+    st.write("Sold this project to an Youtuber for: 40 USD ")
     st.write("Github link: [link](https://github.com/nuttysunday/Covid19-Prediction)")
     video_file1 = open('covid.mp4', 'rb')
     video_bytes1 = video_file1.read()
@@ -43,6 +44,7 @@ st.header("Work Experience:")
 with st.expander("Free lancing (Research Paper): Protocol-Based-Deep-Intrusion-Detection-for-DoS-Normal-and-DDoS-Attacks (04/2022 - 5/2022)"):
     st.write("A free lancing project for the client where i did the coding of the research paper, got accuracy of '93%' on training set, and '87%' on testing set")
     st.write("Technologies Used: Tensorflow, Deep learning, RNN, LSTM, Jupyter Notebook, Upsampling")
+    st.write("Charged: 150 USD")
     st.write("Github link: [link](https://github.com/nuttysunday/Protocol-Based-Deep-Intrusion-Detection-for-DoS-Normal-and-DDoS-Attacks)")
     st.write("Research Paper link: [link](https://github.com/nuttysunday/Protocol-Based-Deep-Intrusion-Detection-for-DoS-Normal-and-DDoS-Attacks/blob/main/1__Protocol-Based_Deep_Intrusion_Detection_for_DoS_and_DDoS_Attacks_Using_UNSW-NB15_and_Bot-IoT_Data-Sets.pdf)")
 
